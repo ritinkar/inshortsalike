7907cdc81f6c39fe603cd78bcfbc95f2fb2613c3
ritinkar pramanik <ritinkar@outlook.com>
2018-01-16 22:27:26 +0530

    MainContainer should not update

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -10,6 +10,11 @@ class MainContainer extends React.Component {
     this.props.fetchData();
   }
 
+  //Since We fetch the dummy data during the first render of MainContainer 
+  //and there's nothing to modify we don't want it to re-render
+  shouldComponentUpdate() {
+      return false;
+  }
 
   render() {
     return (
