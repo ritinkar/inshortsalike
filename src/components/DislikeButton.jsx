043e0a21396893a80b014a68d1c26066c068992d
ritinkar pramanik <ritinkar@outlook.com>
2018-01-17 16:51:59 +0530

    DisLikeButton done

--- src/components/DislikeButton.jsx
+++ src/components/DislikeButton.jsx
@@ -1,14 +1,40 @@
 import React from 'react';
 import FontAwesome from 'react-fontawesome';
+import { dislike } from '../actions/data';
+import { connect } from 'react-redux';
 
 class DislikeButton extends React.Component {
+
     render() {
+        let icon = null;
+        if (this.props.isLiked === -1) {
+            icon = <FontAwesome name='thumbs-down' style={styles.active} />
+        }
+        else {
+            icon = <FontAwesome name='thumbs-down' />
+        }
         return (
-            <div style={{ flex: 1, textAlign: 'center' }}>
-                <FontAwesome name='thumbs-o-down' />
+            <div style={{ flex: 1, textAlign: 'center' }} onClick={() => this.props.onClick(this.props.id)}>
+                {icon}
             </div>
         )
     }
 }
 
-export default DislikeButton;
+const styles = {
+    active: {
+        color: 'green'
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onClick: (id) => {
+            dispatch(dislike(id))
+        }
+    }
+}
+
+
+
+export default connect(null, mapDispatchToProps)(DislikeButton);
